cd6386949e627a5a03c716dbda37e24adf9c3afc
thanhduong <thanh.dd@tinasoft.com.vn>
2024-12-24 12:41:33 +0700

    first commit

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,5 +1,5 @@
 const config = {
-    baseUrl: 'https://foodstore-production-167c.up.railway.app/api',
+    baseUrl: 'http://127.0.0.1:8000/api',
     maxSizeUploadAvatar: 1048576 // bytes
 }
 
--- src/constants/configimg.ts
+++ src/constants/configimg.ts
@@ -1,5 +1,5 @@
 const configImg = {
-    baseUrl: 'https://pushimage-production.up.railway.app/api/',
+    baseUrl: 'http://127.0.0.1:8081/api',
     maxSizeUploadAvatar: 1048576 // bytes
 }
 export default configImg
